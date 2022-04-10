82e128d9574728fb30771344f9898fd5a3a521a6
rachmanridwan <90601251+rachmanridwan@users.noreply.github.com>
2022-04-10 21:18:30 +0700

    Update Soal2.R

--- Soal2.R
+++ Soal2.R
@@ -1,17 +1,18 @@
+#No.2
 Sampel = 20
 Peluang_Sembuh = 0.2
 Peluang_Tidak_Sembuh = 1 - Peluang_Sembuh
-#2a
+#a
 Pasien_Sembuh = 4
 
 dbinom(Pasien_Sembuh, Sampel, Peluang_Sembuh)
 
-#2b
+#b
 peluang <- dbinom(1:20, 20, 0.2)
 data = data.frame(y=c(peluang), x=c(1:20))
 barplot(data$y, names.arg=data$x, ylab="Peluang", xlab="Jumlah Pasien")
 
-#Nomor 2c
+#c
 p = Peluang_Sembuh
 q = Peluang_Tidak_Sembuh
 n = Pasien_Sembuh
