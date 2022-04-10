43fe77c90142b938361550dac555567c05bca579
rachmanridwan <90601251+rachmanridwan@users.noreply.github.com>
2022-04-10 21:29:39 +0700

    Update Soal3.R

--- Soal3.R
+++ Soal3.R
@@ -1,17 +1,19 @@
+#No.3
+
 mean_bayi_lahir = 4.5
-#3a
+#a
 banyak_bayi_lahir = 6
 dpois(banyak_bayi_lahir, mean_bayi_lahir)
 
-#3b
+#b
 peluang = dpois(banyak_bayi_lahir,mean_bayi_lahir)
 data = data.frame(y=c(peluang), x=c(1:365))
 barplot(data$y, names.arg=data$x, ylab="peluang", xlab="hari ke-", ylim=0:1)
 
-#3c
+#c
 # Dari Perhitungan yang diperoleh, didapatkan bahwa nilai distribusi poisson tidak berubah dari hari pertama hingga hari terakhir.
 
-#3d
+#d
 lambda = mean_bayi_lahir
 rataan = varian = lambda
 rataan
