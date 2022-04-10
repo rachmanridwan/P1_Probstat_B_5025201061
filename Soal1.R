9c9258b882d0eafce5a9db1ba2c3b9c592e377aa
rachmanridwan <90601251+rachmanridwan@users.noreply.github.com>
2022-04-10 20:30:12 +0700

    Update Soal1.R

--- Soal1.R
+++ Soal1.R
@@ -6,27 +6,20 @@ p = 0.20
 dgeom(x = n, prob = p)
 
 #b
-mean(rgeom(n = 10000, prob = p) == 3)
+data = 10000
+p = 0.2
+x = 3
+vec = (rgeom(data, p) == x)
+mean(vec)
 
 #c
-#Hasil dari perhitungan a dan b tidak terlalu jauh, bedanya kalau a itu nilainya tetap
-#sedangkan kalau b itu hasilnya random, besar kecil nilainya ditentukan oleh X
+#Hasil dari perhitungan a dan b tidak terlalu jauh, dari 10000 bilangan
 
 #d
-data.frame(x = 0:10, prob = dgeom(x = 0:10, prob = p)) %>%
-  mutate(Failures = ifelse(x == n, n, "other")) %>%
-  ggplot(aes(x = factor(x), y = prob, fill = Failures)) +
-  geom_col() +
-  geom_text(
-    aes(label = round(prob,2), y = prob + 0.01),
-    position = position_dodge(0.9),
-    size = 3,
-    vjust = 0
-  ) +
-  labs(title = "Histogram Distribusi Geometrik,Peluang X = 3 gagal Sebelum Sukses Pertama",
-       subtitle = "Geometric(.2)",
-       x = "Kegagalan sebelum sukses yang pertama (x)",
-       y = "Peluang") 
+set.seed(10)
+x = 10000
+y = rgeom(x, 0.2)
+hist(y)
 
 #e
 #Rataan
